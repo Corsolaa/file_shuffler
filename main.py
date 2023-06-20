6c8ecec72d3693ed01049637e14d252c1dfc9ddd
John <bruno.bouwman4@gmail.com>
2023-06-20 19:04:42 +0200

    Fixed yes/no question
    Made a yes or no question machanism.
    Made a idiot detector.
    Set a testing default for myself.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import os
 
+
 def start_program():
     shuffle_directory = select_directory()
+    if shuffle_directory:
+        print("shuffle, shuffle, shuffle")
+
 
 def select_directory():
     while True:
         print("Please input the directory where to files are located,")
-        shuffle_directory = input(" that you want to shuffle: ")
+        shuffle_directory = input("that you want to shuffle: ")
+        shuffle_directory = r"C:\Users\bruno\Desktop\dmz missions"
         if os.path.isdir(shuffle_directory):
             break
-        print("Please enter a valid directory...")
+        print("Please enter a valid path to a dictionary...")
     print("Are you sure you want to shuffle: " + shuffle_directory)
-    question = input("Please enter Y(es) or N(o): ")
-    return shuffle_directory
-
-def yes_or_no_filter(question):
-    
+    input_counter = 0
+    while True:
+        input_counter += 2
+        question = input("Please enter yes or no: ")
+        if question.lower() == "yes":
+            return True
+        elif question.lower() == "no":
+            return False
+        if input_counter == 5:
+            print("then go away")
+            return False
 
 
 if __name__ == '__main__':
